e5d2a007af76656a608edce25714e24c52d62f70
Greg <gregpchurch@gmail.com>
2018-07-24 20:06:52 -0500

    add ajax get to access projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,35 @@
 $(document).ready(function() {
+  const projectContainer = $('.project-container');
+  const dataURL = './projects.json';
+
+  $.ajax({
+    url: dataURL,
+    method: 'GET'
+  })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => res.status(422).json(err));
+
   $('.menu-icon').on('click', function() {
     $('nav ul').toggleClass('showing');
   });
-});
 
-$(window).on('scroll', function() {
-  if ($(window).scrollTop()) {
-    $('nav').addClass('black scroll');
-    $('.logo a').addClass('scroll');
-    $('.image').addClass('spin');
-  } else {
-    $('nav').removeClass('black scroll');
-    $('.logo a').removeClass('scroll');
-    $('.image').removeClass('spin');
-  }
-});
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop()) {
+      $('nav').addClass('black scroll');
+      $('.logo a').addClass('scroll');
+      $('.image').addClass('spin');
+    } else {
+      $('nav').removeClass('black scroll');
+      $('.logo a').removeClass('scroll');
+      $('.image').removeClass('spin');
+    }
+  });
 
-$('.scrolling-link').click(function(e) {
-  e.preventDefault();
-  let aid = $(this).attr('href');
-  $('html,body').animate({ scrollTop: $(aid).offset().top - 130 }, 'slow');
+  $('.scrolling-link').click(function(e) {
+    e.preventDefault();
+    let aid = $(this).attr('href');
+    $('html,body').animate({ scrollTop: $(aid).offset().top - 120 }, 'slow');
+  });
 });
